801aabc70a82d06798b45a3a00ba7d745bc460b6
Amber Robeck <arr5533@gmail.com>
2021-12-16 19:15:25 -0600

    replaced the uppercaseLetters variable with the userChoice variable in the pleaseWork for loop to generate from all arrays instead of the first testing one.

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,7 @@ generateBtn.addEventListener("click", writePassword);
 function writePassword() {
   // console.log("testing"); to make sure button is working
   var password = generatePassword();
-  console.log(generatePassword)
+  // console.log(generatePassword)
   var passwordText = document.querySelector("#password");
   
   passwordText.value = password;
@@ -21,46 +21,46 @@ function writePassword() {
     alert("You must choose between 8 and 128 characters!")
     return;
   }
-  console.log(numOfChar);
+  // console.log(numOfChar);
   //  to make sure data is stored in num of char
   
   var lowercaseLetters= ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]; 
   var uppercaseLetters= ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
   var specialCharacters= ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"]; 
   var numbers= [1,2,3,4,5,6,7,8,9,0];
-  console.log(uppercaseLetters);
+  // console.log(uppercaseLetters);
   
   //userInput confirmation for numeric, lowercase, uppercase, special (separately)
   var userChoice= [];
   var lowercase = confirm("Would you like to include lowercase letters?");
-  console.log(lowercase);
+  // console.log(lowercase);
    if (lowercase) {
      userChoice=lowercaseLetters.concat(userChoice);
      
-     console.log(userChoice);
+    //  console.log(userChoice);
 
    }
   
   var uppercase = confirm("Would you like to include uppercase letters?");
-  console.log(uppercase);
+  // console.log(uppercase);
   if (uppercase) {
     userChoice=uppercaseLetters.concat(userChoice);
     
-    console.log(userChoice);
+    // console.log(userChoice);
 
   }
 
   var numbersChoice= confirm("Would you like to include numbers?");
-  console.log(numbersChoice);
+  // console.log(numbersChoice);
   if (numbersChoice) {
     userChoice=numbers.concat(userChoice);
     
-    console.log(userChoice);
+    // console.log(userChoice);
 
   }
 
   var special = confirm("Would you like to include special characters?");
-  console.log(special);
+  // console.log(special);
   if (special) {
     userChoice=specialCharacters.concat(userChoice);
     
@@ -72,14 +72,14 @@ function writePassword() {
 
   //save those confirmations in separate variables
   // store all the values in form of arrays
-  var userInput = {
-    userLowercase: lowercase,
-    userUppercase: uppercase,
-    userNumbers: numbersChoice,
-    userSpecial: special,
+  // var userInput = {
+    // userLowercase: lowercase,
+    // userUppercase: uppercase,
+    // userNumbers: numbersChoice,
+    // userSpecial: special,
   
-  }
-  console.log(userInput) 
+  // }
+  // console.log(userInput) 
 
   //concat array
   
@@ -95,7 +95,7 @@ function writePassword() {
 
 var pleaseWork = []; 
   for (var i = 0; i < parseInt(numOfChar); i++) {
-    pleaseWork.push(uppercaseLetters[Math.floor(Math.random() * uppercaseLetters.length)]) 
+    pleaseWork.push(userChoice[Math.floor(Math.random() * userChoice.length)]) 
     console.log(pleaseWork)
     console.log(typeof pleaseWork)
   }
