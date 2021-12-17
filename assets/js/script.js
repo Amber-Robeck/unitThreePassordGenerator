41e2d6b240e77ba264e97f85410b4af80374c8ce
Amber Robeck <arr5533@gmail.com>
2021-12-16 19:30:33 -0600

    deleted unused code, unnecessary comments, added equal operator to numOfChar variable

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,118 +1,63 @@
-// Assignment Code
 var generateBtn = document.querySelector("#generate");
 
 generateBtn.addEventListener("click", writePassword);
 
-// Write password to the #password input
 function writePassword() {
-  // console.log("testing"); to make sure button is working
   var password = generatePassword();
-  // console.log(generatePassword)
   var passwordText = document.querySelector("#password");
-  
   passwordText.value = password;
 }
   
   function generatePassword() {
   
-  //add "prompt function" for userInput for num of char in password
+  //Asks userInput for num of char in password
   var numOfChar = prompt("enter the length of password");
-  if (!(numOfChar >= 8 && numOfChar < 128)) {
+  if (!(numOfChar >= 8 && numOfChar <= 128)) {
     alert("You must choose between 8 and 128 characters!")
     return;
   }
-  // console.log(numOfChar);
-  //  to make sure data is stored in num of char
   
+  //Variable arrays for different choices of characters
   var lowercaseLetters= ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]; 
   var uppercaseLetters= ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
   var specialCharacters= ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"]; 
   var numbers= [1,2,3,4,5,6,7,8,9,0];
-  // console.log(uppercaseLetters);
+
   
   //userInput confirmation for numeric, lowercase, uppercase, special (separately)
+  //If user chooses special characters then concat to array userChoice
   var userChoice= [];
   var lowercase = confirm("Would you like to include lowercase letters?");
-  // console.log(lowercase);
    if (lowercase) {
      userChoice=lowercaseLetters.concat(userChoice);
-     
-    //  console.log(userChoice);
+    }
 
-   }
-  
+  //If user chooses uppercase letters then concat to array userChoice
   var uppercase = confirm("Would you like to include uppercase letters?");
-  // console.log(uppercase);
   if (uppercase) {
     userChoice=uppercaseLetters.concat(userChoice);
-    
-    // console.log(userChoice);
+    }
 
-  }
-
+  //If user chooses number then concat to array userChoice
   var numbersChoice= confirm("Would you like to include numbers?");
-  // console.log(numbersChoice);
   if (numbersChoice) {
     userChoice=numbers.concat(userChoice);
-    
-    // console.log(userChoice);
-
   }
 
+  //If user chooses special characters then concat to array userChoice
   var special = confirm("Would you like to include special characters?");
-  // console.log(special);
   if (special) {
     userChoice=specialCharacters.concat(userChoice);
-    
-    console.log(userChoice);
-
-  }
-
-
-
-  //save those confirmations in separate variables
-  // store all the values in form of arrays
-  // var userInput = {
-    // userLowercase: lowercase,
-    // userUppercase: uppercase,
-    // userNumbers: numbersChoice,
-    // userSpecial: special,
+    }
   
-  // }
-  // console.log(userInput) 
-
-  //concat array
-  
-
-// var random = uppercaseLetters[Math.floor(Math.random() * uppercaseLetters.length)];
-//   console.log("Random:", random);
-// console.log(numOfChar)
-// console.log(typeof numOfChar)
-// console.log(numOfChar.length)
-  
-
-//if I call this password will it enter the password into box
-
-var pleaseWork = []; 
+//Takes concat array and randomly generates password from user choice of characters
+var passwordGenerator = []; 
   for (var i = 0; i < parseInt(numOfChar); i++) {
-    pleaseWork.push(userChoice[Math.floor(Math.random() * userChoice.length)]) 
-    console.log(pleaseWork)
-    console.log(typeof pleaseWork)
+    passwordGenerator.push(userChoice[Math.floor(Math.random() * userChoice.length)]) 
+    console.log(passwordGenerator)
+    console.log(typeof passwordGenerator)
   }
-  
-  // password=pleaseWork.join("");
-  return password=pleaseWork.join("");
-  
-  
-  
-  
- 
-
-      //generate random num for concat array and grab the item
-  //push that item in new password array
-  // return new password
+  return password=passwordGenerator.join("");
 }
- 
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+ //Button for user click
+// generateBtn.addEventListener("click", writePassword);
